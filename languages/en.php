d57b06e2a0cb74d2c7fb1214107280c6fe525251
Jerome Bakker <jeabakker@coldtrick.com>
2014-05-08 11:48:10 +0200

    translations

--- languages/en.php
+++ languages/en.php
@@ -4,6 +4,6 @@ return array(
 	'embed_extended:menu:embed:internal_content' => "Search for content",
 	'embed_extended:internal_content:placeholder' => "Type the title or name to search from",
 	'embed_extended:internal_content:match_owner' => "Only in my content",
-	'embed_extended:internal_content:container' => "Limit to this group",
+	'embed_extended:internal_content:container' => "Content in this group",
 	
 );
---
+++ languages/nl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file was created by Translation Editor v2.0
+ * On 2014-05-08 11:47
+ */
+
+$language = array (
+  'embed_extended:menu:embed:internal_content' => 'Zoek naar content',
+  'embed_extended:internal_content:placeholder' => 'Type de titel of naam om naar te zoeken',
+  'embed_extended:internal_content:match_owner' => 'Alleen mijn content',
+  'embed_extended:internal_content:container' => 'Content in deze groep',
+);
+add_translation("nl", $language);
