c957b54984f79c731ac95e09d276ea703356473c
Jon Altuna <jonaltuna@msn.com>
2018-09-18 14:33:42 +0200

    added: Spanish translation

---
+++ languages/es.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file was created by Translation Editor v6.0
+ * On 2017-10-07 16:52
+ */
+
+return array (
+  'embed_extended:internal_content:match_owner' => 'Solo en mi contenido',
+  'embed_extended:internal_content:container' => 'Contenido en este grupo',
+  'embed_extended:internal_content:placeholder' => 'Escriba el titulo o nombre para buscar (necesario)',
+  'embed_extended:menu:embed:internal_content' => 'Buscar en contenido',
+);
