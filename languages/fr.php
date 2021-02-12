b10fc9f82ee7d876b12dc30afb1e3a472cae9f7f
Jerôme Bakker <jeabakker@coldtrick.com>
2021-02-12 09:40:31 +0100

    added: French translation
    for v 4.0
    
    Co-authored-by: Dan d'Auge <76149200+danhype@users.noreply.github.com>

---
+++ languages/fr.php
@@ -0,0 +1,8 @@
+<?php
+
+return array (
+  'embed_extended:menu:embed:internal_content' => 'Rechercher un contenu',
+  'embed_extended:internal_content:placeholder' => 'Saisissez le titre ou le nom à rechercher',
+  'embed_extended:internal_content:match_owner' => 'Seulement dans mes contenus',
+  'embed_extended:internal_content:container' => 'Contenu du groupe',
+);
